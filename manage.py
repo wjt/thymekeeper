ee5477a5a8d685690325100522099a4f7aa50a7c
Will Thompson <will@willthompson.co.uk>
2015-04-29 07:38:10 +0100

    manager: auto-create instance dir

--- manage.py
+++ manage.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python
+import os
+import errno
 from flask.ext.script import Manager
 
 from thymekeeper import app, db, user_datastore
 
 manager = Manager(app)
 
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+
 @manager.command
 def syncdb():
     """
     Create the database.
     """
+    mkdir_p(app.instance_path)
     db.create_all()
+
 
 @manager.command
 def reset():
@@ -18,7 +31,8 @@ def reset():
     Drops and recreates the database, IRREVOCABLY and WITHOUT CONFIRMATION.
     """
     db.drop_all()
-    db.create_all()
+    syncdb()
+
 
 @manager.command
 def createsuperuser(email, password):
@@ -27,6 +41,7 @@ def createsuperuser(email, password):
 
     The name is cargo-culted from Django so tab-completing manage.py works ;-)
     """
+    syncdb()
     user_datastore.create_user(email=email, password=password)
     db.session.commit()
 
